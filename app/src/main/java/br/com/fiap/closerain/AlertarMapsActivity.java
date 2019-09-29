d73a4da2ab61bb61cee0b2cd10514d9ef9cf0793
RibaSystem <ribamatriba@gmail.com>
2019-09-29 17:34:12 -0300

    localizacao do usuario

--- app/src/main/java/br/com/fiap/closerain/AlertarMapsActivity.java
+++ app/src/main/java/br/com/fiap/closerain/AlertarMapsActivity.java
@@ -2,20 +2,28 @@ package br.com.fiap.closerain;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -27,9 +35,8 @@ public class AlertarMapsActivity extends Fragment implements OnMapReadyCallback 
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
-    public AlertarMapsActivity(){
 
-    }
+    Location currentLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,14 +45,15 @@ public class AlertarMapsActivity extends Fragment implements OnMapReadyCallback 
 
     }
 
+
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container,
-                              Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
         ActivityCompat.requestPermissions(
-                getActivity(),new String[]{
+                getActivity(), new String[]{
                         Manifest.permission.ACCESS_FINE_LOCATION
-                },0
+                }, 0
         );
 
         mView = inflater.inflate(R.layout.activity_alertar_maps, container, false);
@@ -56,7 +64,7 @@ public class AlertarMapsActivity extends Fragment implements OnMapReadyCallback 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mMapView = (MapView) mView.findViewById(R.id.map);
-        if (mMapView != null){
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
@@ -65,31 +73,35 @@ public class AlertarMapsActivity extends Fragment implements OnMapReadyCallback 
     }
 
     @Override
-    public void onMapReady(final GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        mGoogleMap = googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.getUiSettings().setZoomGesturesEnabled(true);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        LatLng fiap = new LatLng(-23.574080, -46.623222);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fiap));
-
-        googleMap.addMarker(new MarkerOptions().position(fiap).title("Marker na FIAP"));
-        CameraPosition cameraPosition = new CameraPosition(fiap, 17, 0, 0);
-        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),3000, null);
-
-    }
-    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (PackageManager.PERMISSION_GRANTED ==
                 ContextCompat.checkSelfPermission(
                         getActivity(), Manifest.permission.ACCESS_FINE_LOCATION
-                )){
+                )) {
             mGoogleMap.setMyLocationEnabled(true);
+
         }
     }
 
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        mGoogleMap.setMyLocationEnabled(true);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+        LatLng fiap = new LatLng(-23.574080, -46.623222);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(fiap));
+
+
+        googleMap.addMarker(new MarkerOptions().position(fiap).title("Marker na FIAP"));
+        CameraPosition cameraPosition = new CameraPosition(fiap, 17, 0, 0);
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 3000, null);
+
+    }
 
 
 }
